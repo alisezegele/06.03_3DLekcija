58f60bf12fe690027bc0cf4b4e1874024dee2b5d
alisezegele <146325058+alisezegele@users.noreply.github.com>
2025-03-13 13:52:53 +0200

    UI + Goblin

--- 06.03_3DLekcija/Assets/Scripts/GameManager.cs
+++ 06.03_3DLekcija/Assets/Scripts/GameManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public Player player;
+    [SerializeField] private TMP_Text playerName, playerHealth, enemyName, enemyHealth;
     public Enemy enemy;
     
     void Start()
+    {
+        playerName.text = player.CharName;
+        enemyName.text = enemy.name;
+        UpdateHealth();
+    }
+
+    private void UpdateHealth()
+    {
+        playerHealth.text = player.health.ToString();
+        enemyHealth.text = enemy.health.ToString();
+    }
+
+    public void DoRound()
     {
         int damage = player.Attack();
         enemy.GetHit(damage);
@@ -15,5 +30,6 @@ public class GameManager : MonoBehaviour
         int enemyDamage = enemy.Attack();
         player.GetHit(enemyDamage);
         enemy.Weapon.ApplyEffect(player);
+        UpdateHealth();
     }
 }
